edb715b5258eeadb018ca188ce11761e7a372842
AmrAdel-Dev <166554196+AmrAdel-Dev@users.noreply.github.com>
2024-04-11 20:17:15 +0200

    Add files via upload

---
+++ JavaScript-Codes.js
@@ -0,0 +1,16 @@
+function remove_AC1_1(){
+    document.getElementById('AC1p').style.marginRight = '-800px';
+    document.getElementById('AC1').style.marginRight = '0px';
+}
+function remove_AC1_2(){
+    document.getElementById('AC1').style.marginRight = '-800px';
+    document.getElementById('AC1p').style.marginRight = '0px';
+}
+function remove_CC_1(){
+    document.getElementById('img2-Capital').style.marginRight = '0px';
+    document.getElementById('img1-Capital').style.marginRight = '-1400px';
+}
+function remove_CC_2(){
+    document.getElementById('img1-Capital').style.marginRight = '0px';
+    document.getElementById('img2-Capital').style.marginRight = '-1400px';
+}
